2fa638403832fecce35e369828d986c869e01e5c
Omar Abumuhfouz <omarabumuhfouz@gmail.com>
2026-06-30 21:21:25 +0300

    Add First Pipeline

--- src/AuthService/AuthService.Api/Program.cs
+++ src/AuthService/AuthService.Api/Program.cs
@@ -1,6 +1,7 @@
 using AuthService.Api.Endpoints;
 using AuthService.Endpoints;
 using AuthService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 using SharedKernel;
 
@@ -25,6 +26,26 @@ app.UseSerilogRequestLogging();
 
 // Prometheus
 app.UseOpenTelemetryPrometheusScrapingEndpoint();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+        
+        if (context.Database.IsRelational())
+        {
+            await context.Database.MigrateAsync();
+        }
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+        throw;
+    }
+}
 
 await SeedData.SeedAsync(app.Services);
 
