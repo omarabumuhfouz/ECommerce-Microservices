8e4b11958aa5994dee2767587e3110364a58a61b
Omar Abumuhfouz <omarabumuhfouz@gmail.com>
2026-06-30 21:28:36 +0300

    Add Auth Pipeline

--- src/AuthService/AuthService.Api/Program.cs
+++ src/AuthService/AuthService.Api/Program.cs
@@ -32,6 +32,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     try
     {
+        // Get Context Service
         var context = services.GetRequiredService<AppDbContext>();
         
         if (context.Database.IsRelational())
