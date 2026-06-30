04df601844dd7b07bbd9eee769235dc271e6d9a4
Omar Abumuhfouz <omarabumuhfouz@gmail.com>
2026-06-30 22:11:32 +0300

    Fix AuthService Pipeline Docker Job Error

--- src/AuthService/AuthService.Api/Program.cs
+++ src/AuthService/AuthService.Api/Program.cs
@@ -32,6 +32,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
     try
     {
+        //
         var context = services.GetRequiredService<AppDbContext>();
         
         if (context.Database.IsRelational())
