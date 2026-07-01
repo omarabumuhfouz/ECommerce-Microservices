535a9e6996a08f0996e4a530a95a2b040bb7d53d
Omar Abumuhfouz <omarabumuhfouz@gmail.com>
2026-07-01 23:43:13 +0300

    First Testing for Notification Service CI/CD Pipeline

--- src/AuthService/AuthService.Infrastructure/Data/AppDbContextFactory.cs
+++ src/AuthService/AuthService.Infrastructure/Data/AppDbContextFactory.cs
@@ -9,7 +9,7 @@ public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     public AppDbContext CreateDbContext(string[] args)
     {
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseSqlServer("Server=localhost,9111;Database=AuthService;User Id=sa;Password=Password12345!;TrustServerCertificate=True;Encrypt=True;");
+        optionsBuilder.UseSqlServer("Server=sqlserver;Database=AuthService;User Id=sa;Password=Password12345!;TrustServerCertificate=True;Encrypt=True;");
 
         return new AppDbContext(optionsBuilder.Options);
     }
